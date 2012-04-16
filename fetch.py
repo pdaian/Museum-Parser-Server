48f55043bb7ab5ff5eed3302bc803b152dc4c25c
Philip Daian <phil@linux.com>
2012-04-16 06:37:07 +0400

    Initial commit, still testing code for now

---
+++ fetch.py
@@ -0,0 +1,28 @@
+from BeautifulSoup import BeautifulSoup as bs
+import urllib2,re
+
+# Exhibit Class, One Instance per Exhibit
+class Exhibit:
+    def __init__(self, title, startdate, enddate, url, image):
+        self.title = title
+        self.startdate = startdate        
+        self.enddate = enddate
+        self.url = url
+        self.image = image
+
+# Museum dictionary, str:list(Exhibit)
+museums = {}
+
+# Fetch and parse MOMA with BeautifulSoup
+url = "http://www.moma.org/explore/exhibitions/on_view"
+content = urllib2.urlopen(url).read()
+soup = bs(content)
+exhibits = soup.findAll('ol')
+assert str(exhibits[0]["class"]) == "exhibitions"
+for exhibit in exhibits.contents:
+    try:
+	exhibit.contents
+    except:
+	# Indicates a malformed entry, possible a newline
+	pass
+
